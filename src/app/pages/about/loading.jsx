ff70a1f1a3c3aa46f00e5873f9d0a9d612b74af1
Sumit Dalai <dalaisumit611@gmail.com>
2024-12-03 15:22:52 +0530

    about page setup by error handler and loading component

---
+++ src/app/pages/about/loading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const loading = () => {
+  return <div>LOading...</div>;
+};
+
+export default loading;
---
+++ src/app/pages/about/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+const Page = async () => {
+  return <div className="bg-red-600">About page</div>;
+};
+
+export default Page;
