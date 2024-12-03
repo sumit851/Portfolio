42d51f399270749cf8aa2ba4610550df9c6bcde2
Sumit Dalai <dalaisumit611@gmail.com>
2024-12-03 15:21:58 +0530

    responsive navbar added

---
+++ src/app/components/navbar/Responsive.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import React, { useState } from "react";
+import { AlignJustify, X } from "lucide-react";
+import Link from "next/link";
+
+const Responsive = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <div className="fixed bottom-4 right-4 z-50">
+        <button
+          onClick={toggleMenu}
+          className="bg-gray-800 p-3 rounded-full shadow-lg text-white focus:outline-none"
+        >
+          {isOpen ? (
+            <X className="h-6 w-6" />
+          ) : (
+            <AlignJustify className="h-6 w-6" />
+          )}
+        </button>
+      </div>
+
+      {isOpen && (
+        <div className="fixed bottom-20 right-4 bg-gray-800 rounded-lg shadow-lg w-48 z-40">
+          <div className="flex flex-col space-y-2 p-4">
+            <Link href="/pages/project">
+              <span className="text-white hover:text-yellow-300 transition-colors cursor-pointer block">
+                Project
+              </span>
+            </Link>
+            <Link href="/pages/about">
+              <span className="text-white hover:text-yellow-300 transition-colors cursor-pointer block">
+                About
+              </span>
+            </Link>
+            <Link href="/">
+              <span className="text-white hover:text-yellow-300 transition-colors cursor-pointer block">
+                Home
+              </span>
+            </Link>
+            <Link href="/pages/contact">
+              <span className="text-white hover:text-yellow-300 transition-colors cursor-pointer block">
+                Contact
+              </span>
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Responsive;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,8 @@
+"use client";
+import Footer from "./components/footer/page";
+import Navbar from "./components/navbar/page";
+import Responsive from "./components/navbar/Responsive";
+import { useWindowSize } from "../hooks/widthcheck";
 import "./globals.css";
 
 export default function RootLayout({
@@ -5,9 +10,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isDesktop = useWindowSize();
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="">
+        {isDesktop ? <Navbar /> : <Responsive />}
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
