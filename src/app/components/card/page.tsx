a9f89af9deaf1a653a7cdceffc7ba2f2f6dffe3e
Sumit Dalai <dalaisumit611@gmail.com>
2024-12-05 08:44:34 +0530

    final commit after all work finished

---
+++ src/app/components/card/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./card.module.css";
+import { CardContainer } from "@/components/ui/3d-card";
+const ProjectCard = () => {
+  return (
+    <>
+      <CardContainer />
+    </>
+  );
+};
+
+export default ProjectCard;
--- src/app/pages/project/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Project = () => {
-  return <div>Project Section</div>;
-};
-
-export default Project;
