4f42f74dad811024a48654034ce0ba869dc583f3
Sumit Dalai <dalaisumit611@gmail.com>
2024-12-05 08:43:03 +0530

    project page finished

---
+++ src/app/pages/project/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ProjectCard from "@/app/components/card/page";
+const Project = () => {
+  return (
+    <div>
+      <ProjectCard />
+    </div>
+  );
+};
+
+export default Project;
