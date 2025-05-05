009f0529220c4ed2e0a1a0b67d725ae76371fb38
sumodalai61 <sumodalai61@gmail.com>
2025-05-05 07:21:02 +0000

    add research and blog serction link on navbar

--- src/app/components/navbar/page.tsx
+++ src/app/components/navbar/page.tsx
@@ -4,22 +4,28 @@ import Link from "next/link";
 import "./navbar.module.css";
 const Navbar = () => {
   return (
-    <nav className="bg-gray-500 rounded-lg mx-auto mt-4 w-11/12 max-w-4xl">
-      <div className="flex justify-around items-center h-16">
+    <nav className="navbar">
+      <div className="nav-container">
         <Link
-          className="text-white text-xl"
-          href="https://github.com/sumit851/"
+          className="nav-link"
+          href="/pages/project"
         >
           Project
         </Link>
-        <Link className="text-white text-xl" href="/pages/about">
+        <Link className="nav-link" href="/pages/about">
           About
         </Link>
-        <Link className="text-white text-xl" href="/">
+        <Link className="nav-link" href="/">
           Home
         </Link>
-        <Link className="text-white text-xl" href="/pages/contact">
-          contact
+        <Link className="nav-link" href="/pages/ml-research">
+         Research
+        </Link>
+        <Link className="nav-link" href="/pages/blog">
+          Blog
+        </Link>
+        <Link className="nav-link" href="/pages/contact">
+          Contact
         </Link>
       </div>
     </nav>
